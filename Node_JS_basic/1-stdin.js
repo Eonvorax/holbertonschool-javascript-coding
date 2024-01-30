241a80a7203d5757dd42c06326eccb97d1f1a787
Eonvorax <mail4bishop@gmail.com>
2024-01-30 14:47:58 +0100

    Task 1: Using Process stdin

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+process.stdin.setEncoding('utf8');
+
+const welcomeMessage = 'Welcome to Holberton School, what is your name?';
+process.stdout.write(`${welcomeMessage}\n`);
+
+process.stdin.on('readable', () => {
+    const userInput = process.stdin.read();
+    if (userInput) {
+        process.stdout.write(`Your name is: ${userInput}`);
+    }
+    process.stdout.write('This important software is now closing\n');
+
+    process.exit()
+});
